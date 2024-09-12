559684cdcc6cb66d0dc29dbe68c0e6afa3a2b783
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-09-12 15:59:00 +0100

    Move record to Emulator and add spdlog::error

--- CR_Bot-CPP/src/Emulator.hpp
+++ CR_Bot-CPP/src/Emulator.hpp
@@ -7,11 +7,33 @@
 #include <boost/dll.hpp>
 #include <boost/process.hpp>
 
+#include <spdlog/spdlog.h>
+
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
+#include <tobiaslocker_base64/base64.hpp>
+
+#include "video/CodecContext.hpp"
+
+//static std::string screen_record_bash = R"(#!/bin/bash
+//while true; do
+//    screenrecord --output-format=h264 \
+//    --time-limit "179" \
+//    --size "720x1280" \
+//    --bit-rate "5M" -
+//done
+//)";
+
 class Emulator {
 public:
+	Emulator(const std::string device_serial): device_serial(device_serial) {
+	}
+
+	~Emulator() {
+
+	}
+
 	void click(int x, int y) {
 		this->run_adb_command({ "shell", "input", "tap", std::to_string(x), std::to_string(y) });
 	}
@@ -26,11 +48,71 @@ public:
 
 	[[nodiscard]]
 	std::pair<int, int> get_size(const std::string device_serial) {
-		std::string size_str = this->run_adb_command_with_output(device_serial, { "shell", "wm", "size" });
+		std::string size_str = this->run_adb_command_with_output(this->device_serial, { "shell", "wm", "size" });
 		size_str = size_str.substr(std::string("Physical size: ").size(), size_str.size());
 		int width = std::stoi(size_str.substr(0, size_str.find("x")));
 		int height = std::stoi(size_str.substr(size_str.find("x") + 1, size_str.size()));
 		return std::make_pair(width, height);
+	}
+
+	void record() {
+		std::unique_ptr<CodecContext> codec_context = std::make_unique<CodecContext>();
+
+		//std::string record_bash_base64 = base64::to_base64(screen_record_bash);
+		std::vector<std::string> commands = { "exec-out", "screenrecord", "--output-format=h264", "-" };
+
+		std::string command_str = fmt::format("adb -s {} {}", this->device_serial, fmt::join(commands, " "));
+		std::cout << command_str << std::endl;
+
+		boost::asio::io_context ioc;
+		boost::process::async_pipe pipe(ioc);
+		boost::process::child c(command_str, boost::process::std_out > pipe, ioc);
+
+		int frame_number = 0;
+
+		VideoReformatter reformatter;
+		std::array<uint8_t, 2048> buffer;
+		std::function<void()> do_read;
+		do_read = [&]() {
+			boost::asio::async_read(pipe, boost::asio::buffer(buffer),
+				[&](boost::system::error_code ec, std::size_t length) {
+					if (!ec) {
+						// Process the received line
+						std::vector<std::unique_ptr<Packet>> packets = codec_context->parse(buffer.data(), buffer.size());
+						if (packets.empty()) {
+							do_read();
+							return;
+						}
+
+						std::unique_ptr<Packet>& packet = packets.back();
+
+						std::vector<std::unique_ptr<VideoFrame>> frames = codec_context->decode(packet);
+
+						for (std::unique_ptr<VideoFrame>& frame : frames) {
+							std::unique_ptr<VideoFrame> reformatted_frame = reformatter.reformat(frame.get(), 720, 1280, AV_PIX_FMT_BGR24);
+							if (!reformatted_frame) {
+								continue;
+							}
+
+							cv::Mat image = reformatted_frame->to_image();
+							cv::imwrite(fmt::format("frame_{}.png", frame_number), image);
+
+							frame_number++;
+						}
+
+						do_read();  // Initiate the next asynchronous read
+					}
+					else {
+						spdlog::error("Error: {}", ec.message());
+					}
+				}
+			);
+			};
+
+		// Start the first read
+		do_read();
+
+		ioc.run();
 	}
 
 	void run_adb_command(const std::vector<std::string> commands) {
--- CR_Bot-CPP/src/main.cpp
+++ CR_Bot-CPP/src/main.cpp
@@ -1,112 +1,10 @@
-#include <iostream>
-
-#include <boost/iostreams/device/array.hpp>
-#include <boost/iostreams/stream_buffer.hpp>
-#include <boost/asio.hpp>
-
 #include <boost/dll.hpp>
-#include <boost/process.hpp>
-
 #include <toml++/toml.hpp>
 
+#include <spdlog/spdlog.h>
 #include <fmt/format.h>
-#include <fmt/ranges.h>
 
-#include <tobiaslocker_base64/base64.hpp>
-
-#include <opencv2/opencv.hpp>
-#include <png.h>
-
-#include "video/CodecContext.hpp"
-
-#include "video/VideoFrame.hpp"
-#include "video/Packet.hpp"
-
-#include "video/VideoReformatter.hpp"
-#include "video/VideoFormat.hpp"
-
-//#include "Emulator.hpp"
-
-static std::string screen_record_bash = R"(#!/bin/bash
-while true; do
-    screenrecord --output-format=h264 \
-    --time-limit "179" \
-    --size "720x1280" \
-    --bit-rate "5M" -
-done
-)";
-
-
-static std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
-	size_t start_pos = 0;
-	while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
-		str.replace(start_pos, from.length(), to);
-		start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
-	}
-	return str;
-}
-
-
-static void record(const std::string device_serial) {
-	//av_log_set_level(AV_LOG_QUIET);
-	std::unique_ptr<CodecContext> codec_context = std::make_unique<CodecContext>();
-
-	std::string record_bash_base64 = base64::to_base64(screen_record_bash);
-	std::vector<std::string> commands = { "exec-out", "screenrecord", "--output-format=h264", "-" };
-
-	std::string command_str = fmt::format("adb -s {} {}", device_serial, fmt::join(commands, " "));
-	std::cout << command_str << std::endl;
-
-	boost::asio::io_context ioc;
-	boost::process::async_pipe pipe(ioc);
-	boost::process::child c(command_str, boost::process::std_out > pipe, ioc);
-
-	int frame_number = 0;
-
-	VideoReformatter reformatter;
-	std::array<uint8_t, 2048> buffer;
-	std::function<void()> do_read;
-	do_read = [&]() {
-		boost::asio::async_read(pipe, boost::asio::buffer(buffer),
-			[&](boost::system::error_code ec, std::size_t length) {
-				if (!ec) {
-					// Process the received line
-					std::vector<std::unique_ptr<Packet>> packets = codec_context->parse(buffer.data(), buffer.size());
-					if (packets.empty()) {
-						do_read();
-						return;
-					}
-
-					std::unique_ptr<Packet>& packet = packets.back();
-
-					std::vector<std::unique_ptr<VideoFrame>> frames = codec_context->decode(packet);
-
-					for (std::unique_ptr<VideoFrame>& frame : frames) {
-						std::unique_ptr<VideoFrame> reformatted_frame = reformatter.reformat(frame.get(), 720, 1280, AV_PIX_FMT_BGR24);
-						if (!reformatted_frame) {
-							continue;
-						}
-
-						cv::Mat image = reformatted_frame->to_image();
-						cv::imwrite(fmt::format("frame_{}.png", frame_number), image);
-
-						frame_number++;
-					}
-
-					do_read();  // Initiate the next asynchronous read
-				}
-				else {
-					std::cout << "Error: " << ec.message() << std::endl;
-				}
-			}
-		);
-		};
-
-	// Start the first read
-	do_read();
-
-	ioc.run();
-}
+#include "Emulator.hpp"
 
 int main()
 {
@@ -115,17 +13,19 @@ int main()
 
 	auto maybe_ip = config_toml["adb"]["ip"].value<std::string>();
 	if (!maybe_ip) {
-		std::cerr << "No IP provided" << std::endl;
+		spdlog::error("No IP provided");
 		return 1;
 	}
 	auto maybe_device_serial = config_toml["adb"]["device_serial"].value<std::string>();
 	if (!maybe_device_serial) {
-		std::cerr << "No device serial provided" << std::endl;
+		spdlog::error("No device serial provided");
 		return 1;
 	}
 	const std::string ip = maybe_ip.value_or("");
 	const std::string device_serial = maybe_device_serial.value_or("");
-	record(device_serial);
+
+	Emulator emulator = Emulator(device_serial);
+	emulator.record();
 
 	std::cout << "Done";
 	return 0;
